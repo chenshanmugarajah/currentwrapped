fc9708b33855d340b446693b6de6be83cc295a74
Chen <60476123+chenshanmugarajah@users.noreply.github.com>
2023-02-02 22:35:53 +0000

    restart

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-    return (
-        <div>
-            <h1>Current Wrapped</h1>
-            <p>See who you spotify wrapped would look like if it was made today</p>
-        </div>
-    )
-}
